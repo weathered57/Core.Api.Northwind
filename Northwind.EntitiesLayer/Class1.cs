c11ed156b848e34a169acf0c53716cc8610a3959
weathered57 <a_canozdogru57@hotmail.com>
2020-04-11 20:30:29 +0300

    Create Product Entity

--- Northwind.EntitiesLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Northwind.EntitiesLayer
-{
-    public class Class1
-    {
-    }
-}
---
+++ Northwind.EntitiesLayer/Concrete/Product.cs
@@ -0,0 +1,12 @@
+namespace Northwind.EntitiesLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
