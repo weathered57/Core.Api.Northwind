dd09cae5df95a9b1675efb3cc4036c67f57ab8c9
weathered57 <a_canozdogru57@hotmail.com>
2020-04-11 20:06:41 +0300

    first commit

---
+++ Northwind.BusinessLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Northwind.BusinessLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ Northwind.DataAccessLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Northwind.DataAccessLayer
+{
+    public class Class1
+    {
+    }
+}
