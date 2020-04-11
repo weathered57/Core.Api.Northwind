0a053bdd00700fe24d506f11a7a4a86b88f57827
weathered57 <a_canozdogru57@hotmail.com>
2020-04-11 23:23:03 +0300

    Add Context and  Ef Core 3.1.3

---
+++ Northwind.DataAccessLayer/Concrete/EntitityFramework/Context/NorthwindContext.cs
@@ -0,0 +1,24 @@
+namespace Northwind.DataAccessLayer.Concrete.EntitityFramework.Context
+{
+    public class NorthwindContext
+    {
+        public class NorthwindContext : DbContext
+    {
+     
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433\\Catalog=Northwind;Database=Northwind;User=sa;Password=DockerSql123;");
+        }
+     
+
+        public DbSet<Product> Products { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products");
+            
+        }
+    }
+    }
+}
