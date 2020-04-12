cac807e42cdbbce2910006f11e8bf3b1f36ff5ce
weathered57 <a_canozdogru57@hotmail.com>
2020-04-12 18:45:34 +0300

    Added Autofac

---
+++ Northwind.BusinessLayer/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,21 @@
+using Autofac;
+using Northwind.BusinessLayer.Concrete;
+using Northwind.BusinessLayer.Abstract;
+using Northwind.DataAccessLayer.Concrete.EntitityFramework;
+using Northwind.DataAccessLayer.Abstract;
+
+namespace Northwind.BusinessLayer.DependencyResolvers.Autofac
+{
+  public class AutofacBusinessModule : Module
+    {
+        public AutofacBusinessModule()
+        {
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+            builder.RegisterType<ProductManager>().As<IProductService>();           
+        }
+    }
+}
--- Northwind.DataAccessLayer/Concrete/EntitityFramework/EfProductDal.cs
+++ Northwind.DataAccessLayer/Concrete/EntitityFramework/EfProductDal.cs
@@ -2,7 +2,9 @@ using System;
 using Northwind.Core.DataAccess.EntitiyFramework;
 using Northwind.EntitiesLayer.Concrete;
 using Northwind.DataAccessLayer.Abstract;
-namespace Northwind.DataAccessLayer.Concrete.EntitityFramework.Context
+using Northwind.DataAccessLayer.Concrete.EntitityFramework.Context;
+
+namespace Northwind.DataAccessLayer.Concrete.EntitityFramework
 {
    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
