a4406919bb3bfbc869426b03e84b033cf31559d4
weathered57 <a_canozdogru57@hotmail.com>
2020-04-16 13:18:19 +0300

    Added UserForRegisterDto

---
+++ Northwind.EntitiesLayer/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+namespace Northwind.EntitiesLayer.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
