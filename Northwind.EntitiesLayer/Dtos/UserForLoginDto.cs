b727441313c4cfe838c1343fa6b1d267144205f7
weathered57 <a_canozdogru57@hotmail.com>
2020-04-16 13:17:51 +0300

    Added UserForLoginDto

---
+++ Northwind.EntitiesLayer/Dtos/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+namespace Northwind.EntitiesLayer.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
