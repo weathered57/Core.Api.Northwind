f4fd64dc909da42a36d786e87bad6bef20606de8
weathered57 <a_canozdogru57@hotmail.com>
2020-04-16 12:52:53 +0300

    Added UserForOperationClaimDto

---
+++ Northwind.Core/Entities/Concrete/UserForOperationClaimDto.cs
@@ -0,0 +1,9 @@
+namespace Northwind.Core.Entities.Concrete
+{
+    public class UserForOperationClaimDto : IDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
