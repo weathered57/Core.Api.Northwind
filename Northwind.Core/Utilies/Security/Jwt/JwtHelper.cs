25fef0eea4b25eecd5181621a5c2903b56e87199
weathered57 <a_canozdogru57@hotmail.com>
2020-04-17 13:00:12 +0300

    Editin User

--- Northwind.Core/Utilies/Security/Jwt/JwtHelper.cs
+++ Northwind.Core/Utilies/Security/Jwt/JwtHelper.cs
@@ -59,7 +59,7 @@ namespace Northwind.Core.Utilies.Security.Jwt
                 var claims = new List<Claim>();
                 claims.AddNameIdentifier(user.Id.ToString());
                 claims.AddEmail(user.Email);
-                claims.AddName($"{user.FirstName} {user.LastName}");
+                claims.AddName($"{user.Firstname} {user.LastName}");
                 claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
                 return claims;
             }
--- Northwind.EntitiesLayer/Concrete/User.cs
+++ Northwind.EntitiesLayer/Concrete/User.cs
@@ -4,7 +4,7 @@ namespace Northwind.EntitiesLayer.Concrete
     public class User : IEntity
     {
         public int Id { get; set; }
-         public string Firstaname { get; set; }
+         public string Firstname { get; set; }
          public string Lastname { get; set; }
          public string Email { get; set; }
          public byte[] PasswordSalt { get; set; }
