71364f7e0ec88969cb5477e3b9b9940372db05cc
weathered57 <a_canozdogru57@hotmail.com>
2020-04-19 18:35:42 +0300

    Add ValidationTools and FluentValidation.AspNetCore package

---
+++ Northwind.Core/CrossCuttingCorcerns/Validation/ValidationTools.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+namespace Northwind.Core.CrossCuttingCorcerns.Validation
+{
+    public class ValidationTools
+    {
+         public static void FluentValidate(IValidator validator,object entity)
+        {
+            var result = validator.Validate(entity);
+            if(result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
