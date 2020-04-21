63a9224e38d009b45a25dcb5c1fbccdcacd9911e
weathered57 <a_canozdogru57@hotmail.com>
2020-04-21 21:14:42 +0300

    Remove Castle.DynamicProxy from Core and add Autofac.Extras.DynamicProxy to Core and Business

--- Northwind.BusinessLayer/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Northwind.BusinessLayer/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -24,6 +24,9 @@ namespace Northwind.BusinessLayer.DependencyResolvers.Autofac
              builder.RegisterType<AuthManager>().As<IAuthService>();
              builder.RegisterType<JwtHelper>().As<ITokenHelper>();
              
+             var assembly=System.Reflection.Assembly.GetExecutingAssembly();
+             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+             .EnableInterfaceInterceptors
         }
     }
 }
