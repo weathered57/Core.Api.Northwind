702a6156945e06c7d838d1353627376d7bea4f5f
weathered57 <a_canozdogru57@hotmail.com>
2020-04-23 18:12:05 +0300

    Added TransactionScopeAspect

---
+++ Northwind.Core/Aspects/Autofac/TransactionScopeAspect.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using Castle.DynamicProxy;
+using Northwind.Core.Utilies.Interceptors; 
+namespace Northwind.Core.Aspects.Autofac
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (System.Exception e)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
