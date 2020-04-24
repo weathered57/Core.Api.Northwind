25d67a4b8301c49bda16e7f8bb7d1cc5291aa5fd
weathered57 <a_canozdogru57@hotmail.com>
2020-04-24 21:46:44 +0300

    Added Category Controller

---
+++ Northwind.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.BusinessLayer.Abstract;
+using Northwind.Core.Utilies.Results;
+using Northwind.EntitiesLayer.Concrete;
+namespace Northwind.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result = _categoryService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
